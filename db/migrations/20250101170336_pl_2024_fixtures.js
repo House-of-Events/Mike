141715e7e1c8d37c3b9c88409d9372387eaaf37b
Manan <157731770+mananshah78424@users.noreply.github.com>
2025-01-20 20:07:37 -0800

    Restoring files without sensitive data (#2)
    Co-authored-by: Manan Shah <manan@Manans-MacBook-Pro.local>

--- db/migrations/20250101170336_pl_2024_fixtures.js
+++ db/migrations/20250101170336_pl_2024_fixtures.js
@@ -4,13 +4,21 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable('soccer_2024_pl_fixtures', (table) => {
-        table.increments('id').primary().unique();
+        table.string('id')
+            .primary()
+            .defaultTo(knex.raw("concat('mat_', lpad(floor(random() * 1000000)::text, 6, '0'))"));
+        table.string('fixture_id');
+        table.string('match_id');
         table.string('home_team');
         table.string('away_team');
-        table.string('city');
-        table.string('stadium');
-        table.dateTime('date');
+        table.string('venue');
+        table.date('date');
+        table.string('time')
+        table.dateTime('date_time');
+        table.dateTime('date_created').defaultTo(knex.fn.now());
         table.string('league').defaultTo('Premier League');
+        table.boolean('informed').defaultTo(false);
+        table.string('notification_sent_at');
     });
 };
 
